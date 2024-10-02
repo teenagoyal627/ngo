011d4d1a1420b6084417b50d8b8cf96e2f064dec
teenagoyal627 <teenagoyal627@gmail.com>
2024-10-02 13:51:19 +0530

    update the search_value also

--- server/modals/Schema.js
+++ server/modals/Schema.js
@@ -51,50 +51,99 @@ const ReactFormDataSchema = new mongoose.Schema({
 
 });
 
-ReactFormDataSchema.pre("save", function (next) {
-  const doc = this;
-  
+ReactFormDataSchema.pre("findOneAndUpdate", async function (next) {
+  const update = this.getUpdate();
+
+  // Find the current document to get the existing values
+  const docToUpdate = await this.model.findOne(this.getQuery());
+
+  // Create a search array with updated or existing values
   const searchArray = [
-    doc.UserId,
-    doc.RegistrationNo,
-    doc.Name,
-    doc.FatherName,
-    doc.Gender,
-    doc.Address,
-    doc.RegistrationDate,
-    doc.MeanOfTransportation,
-    doc.BroughtBy?.Name,         
-    doc.BroughtBy?.Address,      
-    doc.BroughtBy?.MobileNumber,
-    doc.BroughtBy?.Aadhar,
-    doc.PatientCondition,
-    doc.LanguageKnown,
-    doc.HospitalDepartment,
-    doc.AnandamCenter,
-    doc.SentToHome,
-    doc.OPD,
-    doc.InmateNumber,
-    doc.IONumber,
-    doc.IOName,
-    doc.AadharNumber
+    update.UserId || docToUpdate.UserId,
+    update.RegistrationNo || docToUpdate.RegistrationNo,
+    update.Name || docToUpdate.Name,
+    update.FatherName || docToUpdate.FatherName,
+    update.Gender || docToUpdate.Gender,
+    update.Address || docToUpdate.Address,
+    update.RegistrationDate || docToUpdate.RegistrationDate,
+    update.MeanOfTransportation || docToUpdate.MeanOfTransportation,
+    (update.BroughtBy && update.BroughtBy.Name) || docToUpdate.BroughtBy?.Name,
+    (update.BroughtBy && update.BroughtBy.Address) || docToUpdate.BroughtBy?.Address,
+    (update.BroughtBy && update.BroughtBy.MobileNumber) || docToUpdate.BroughtBy?.MobileNumber,
+    (update.BroughtBy && update.BroughtBy.Aadhar) || docToUpdate.BroughtBy?.Aadhar,
+    update.PatientCondition || docToUpdate.PatientCondition,
+    update.LanguageKnown || docToUpdate.LanguageKnown,
+    update.HospitalDepartment || docToUpdate.HospitalDepartment,
+    update.AnandamCenter || docToUpdate.AnandamCenter,
+    update.SentToHome || docToUpdate.SentToHome,
+    update.OPD || docToUpdate.OPD,
+    update.InmateNumber || docToUpdate.InmateNumber,
+    update.IONumber || docToUpdate.IONumber,
+    update.IOName || docToUpdate.IOName,
+    update.AadharNumber || docToUpdate.AadharNumber
   ];
 
-  if (doc.ImageUrl) {
-    searchArray.push(doc.ImageUrl);
+  if (update.ImageUrl || docToUpdate.ImageUrl) {
+    searchArray.push(update.ImageUrl || docToUpdate.ImageUrl);
   }
 
-  if (doc.PatientsDocuments && doc.PatientsDocuments.length > 0) {
-    doc.PatientsDocuments.forEach((document) => {
+  if (update.PatientsDocuments || docToUpdate.PatientsDocuments) {
+    (update.PatientsDocuments || docToUpdate.PatientsDocuments).forEach((document) => {
       searchArray.push(document.name, document.url);
     });
   }
 
-  doc.Search_value = searchArray
-    .filter(Boolean)    
-    .join("\+");        
+  // Update Search_value
+  update.Search_value = searchArray.filter(Boolean).join("+");
 
   next();
 });
+
+
+// ReactFormDataSchema.pre("save", function (next) {
+//   const doc = this;
+  
+//   const searchArray = [
+//     doc.UserId,
+//     doc.RegistrationNo,
+//     doc.Name,
+//     doc.FatherName,
+//     doc.Gender,
+//     doc.Address,
+//     doc.RegistrationDate,
+//     doc.MeanOfTransportation,
+//     doc.BroughtBy?.Name,         
+//     doc.BroughtBy?.Address,      
+//     doc.BroughtBy?.MobileNumber,
+//     doc.BroughtBy?.Aadhar,
+//     doc.PatientCondition,
+//     doc.LanguageKnown,
+//     doc.HospitalDepartment,
+//     doc.AnandamCenter,
+//     doc.SentToHome,
+//     doc.OPD,
+//     doc.InmateNumber,
+//     doc.IONumber,
+//     doc.IOName,
+//     doc.AadharNumber
+//   ];
+
+//   if (doc.ImageUrl) {
+//     searchArray.push(doc.ImageUrl);
+//   }
+
+//   if (doc.PatientsDocuments && doc.PatientsDocuments.length > 0) {
+//     doc.PatientsDocuments.forEach((document) => {
+//       searchArray.push(document.name, document.url);
+//     });
+//   }
+
+//   doc.Search_value = searchArray
+//     .filter(Boolean)    
+//     .join("\+");        
+
+//   next();
+// });
 
 const User = mongoose.model("ngoportal", ReactFormDataSchema);
 module.exports = User;
