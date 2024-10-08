abdcf2c96bc6cd3e713c9b459d38e258a51eeda8
teenagoyal627 <teenagoyal627@gmail.com>
2024-10-08 22:41:17 +0530

    add search count

--- server/modals/Schema.js
+++ server/modals/Schema.js
@@ -50,51 +50,162 @@ const ReactFormDataSchema = new mongoose.Schema({
 
 });
 
-ReactFormDataSchema.pre("findOneAndUpdate",async function (next){
-  const update=this.getUpdate();
-  const docToUpdate=await this.model.find(this.getQuery())
+// ReactFormDataSchema.pre("findOneAndUpdate",async function (next){
+//   const update=this.getUpdate();
+//   const docToUpdate=await this.model.find(this.getQuery())
 
-  const searchArray=[
-    update.UserId || docToUpdate.UserId,
-    update.RegistrationNo || docToUpdate.RegistrationNo,
-    update.Name || docToUpdate.Name,
-    update.FatherName || docToUpdate.FatherName,
-    update.Gender || docToUpdate.Gender,
-    update.Address || docToUpdate.Address,
-    update.RegistrationDate || docToUpdate.RegistrationDate,
-    update.MeanOfTransportation || docToUpdate.MeanOfTransportation,
-    (update.BroughtBy && update.BroughtBy.Name) || docToUpdate.BroughtBy?.Name,
-    (update.BroughtBy && update.BroughtBy.Address) || docToUpdate.BroughtBy?.Address,
-    (update.BroughtBy && update.BroughtBy.MobileNumber) || docToUpdate.BroughtBy?.MobileNumber,
-    (update.BroughtBy && update.BroughtBy.Aadhar) || docToUpdate.BroughtBy?.Aadhar,
-    update.PatientCondition || docToUpdate.PatientCondition,
-    update.LanguageKnown || docToUpdate.LanguageKnown,
-    update.HospitalDepartment || docToUpdate.HospitalDepartment,
-    update.AnandamCenter || docToUpdate.AnandamCenter,
-    update.SentToHome || docToUpdate.SentToHome,
-    update.OPD || docToUpdate.OPD,
-    update.InmateNumber || docToUpdate.InmateNumber,
-    update.IONumber || docToUpdate.IONumber,
-    update.IOName || docToUpdate.IOName,
-    update.AadharNumber || docToUpdate.AadharNumber,
-    update.State || docToUpdate.State
+//   const searchArray=[
+//     update.UserId || docToUpdate.UserId,
+//     update.RegistrationNo || docToUpdate.RegistrationNo,
+//     update.Name || docToUpdate.Name,
+//     update.FatherName || docToUpdate.FatherName,
+//     update.Gender || docToUpdate.Gender,
+//     update.Address || docToUpdate.Address,
+//     update.RegistrationDate || docToUpdate.RegistrationDate,
+//     update.MeanOfTransportation || docToUpdate.MeanOfTransportation,
+//     (update.BroughtBy && update.BroughtBy.Name) || docToUpdate.BroughtBy?.Name,
+//     (update.BroughtBy && update.BroughtBy.Address) || docToUpdate.BroughtBy?.Address,
+//     (update.BroughtBy && update.BroughtBy.MobileNumber) || docToUpdate.BroughtBy?.MobileNumber,
+//     (update.BroughtBy && update.BroughtBy.Aadhar) || docToUpdate.BroughtBy?.Aadhar,
+//     update.PatientCondition || docToUpdate.PatientCondition,
+//     update.LanguageKnown || docToUpdate.LanguageKnown,
+//     update.HospitalDepartment || docToUpdate.HospitalDepartment,
+//     update.AnandamCenter || docToUpdate.AnandamCenter,
+//     update.SentToHome || docToUpdate.SentToHome,
+//     update.OPD || docToUpdate.OPD,
+//     update.InmateNumber || docToUpdate.InmateNumber,
+//     update.IONumber || docToUpdate.IONumber,
+//     update.IOName || docToUpdate.IOName,
+//     update.AadharNumber || docToUpdate.AadharNumber,
+//     update.State || docToUpdate.State
 
-  ]
+//   ]
 
-  if(update.ImageUrl || docToUpdate.ImageUrl){
-    searchArray.push(update.ImageUrl || docToUpdate.ImageUrl)
+//   if(update.ImageUrl || docToUpdate.ImageUrl){
+//     searchArray.push(update.ImageUrl || docToUpdate.ImageUrl)
+//   }
+
+//   if(update.PatientsDocuments || docToUpdate.PatientsDocuments){
+//     (update.PatientsDocuments || docToUpdate.PatientsDocuments).forEach((document)=>{
+//       searchArray.push(document.name,document.url)
+//     })
+//   }
+
+//   update.Search_value=searchArray.filter(Boolean).join('+')
+//   next()
+
+// })
+
+
+ReactFormDataSchema.pre("save", function (next) {
+  const searchArray = [
+    this.UserId,
+    this.RegistrationNo,
+    this.Name,
+    this.FatherName,
+    this.Gender,
+    this.Address,
+    this.RegistrationDate,
+    this.MeanOfTransportation,
+    this.BroughtBy?.Name,
+    this.BroughtBy?.Address,
+    this.BroughtBy?.MobileNumber,
+    this.BroughtBy?.Aadhar,
+    this.PatientCondition,
+    this.LanguageKnown,
+    this.HospitalDepartment,
+    this.AnandamCenter,
+    this.SentToHome,
+    this.OPD,
+    this.InmateNumber,
+    this.IONumber,
+    this.IOName,
+    this.AadharNumber,
+    this.State,
+    this.ImageUrl,
+  ];
+
+  // Include PatientsDocuments details
+  if (this.PatientsDocuments && this.PatientsDocuments.length > 0) {
+    this.PatientsDocuments.forEach((document) => {
+      if (document.name) searchArray.push(document.name);
+      if (document.url) searchArray.push(document.url);
+    });
   }
 
-  if(update.PatientsDocuments || docToUpdate.PatientsDocuments){
-    (update.PatientsDocuments || docToUpdate.PatientsDocuments).forEach((document)=>{
-      searchArray.push(document.name,document.url)
-    })
+  // Create the Search_value by joining non-falsy values with '+'
+  this.Search_value = searchArray.filter(Boolean).join("+");
+  next();
+});
+
+// Middleware to handle the updating of Search_value before findOneAndUpdate
+ReactFormDataSchema.pre("findOneAndUpdate", async function (next) {
+  try {
+    // Get the update object
+    const update = this.getUpdate();
+
+    // Fetch the current document based on the query
+    const docToUpdate = await this.model.findOne(this.getQuery());
+
+    if (!docToUpdate) {
+      return next(new Error("Document not found."));
+    }
+
+    // Merge existing fields with updated fields
+    const mergedData = {
+      ...docToUpdate.toObject(),
+      ...update,
+      BroughtBy: {
+        ...docToUpdate.BroughtBy,
+        ...(update.BroughtBy || {}),
+      },
+      PatientsDocuments: update.PatientsDocuments || docToUpdate.PatientsDocuments,
+    };
+
+    // Construct the searchArray with updated values
+    const searchArray = [
+      mergedData.UserId,
+      mergedData.RegistrationNo,
+      mergedData.Name,
+      mergedData.FatherName,
+      mergedData.Gender,
+      mergedData.Address,
+      mergedData.RegistrationDate,
+      mergedData.MeanOfTransportation,
+      mergedData.BroughtBy?.Name,
+      mergedData.BroughtBy?.Address,
+      mergedData.BroughtBy?.MobileNumber,
+      mergedData.BroughtBy?.Aadhar,
+      mergedData.PatientCondition,
+      mergedData.LanguageKnown,
+      mergedData.HospitalDepartment,
+      mergedData.AnandamCenter,
+      mergedData.SentToHome,
+      mergedData.OPD,
+      mergedData.InmateNumber,
+      mergedData.IONumber,
+      mergedData.IOName,
+      mergedData.AadharNumber,
+      mergedData.State,
+      mergedData.ImageUrl,
+    ];
+
+    // Include PatientsDocuments details
+    if (mergedData.PatientsDocuments && mergedData.PatientsDocuments.length > 0) {
+      mergedData.PatientsDocuments.forEach((document) => {
+        if (document.name) searchArray.push(document.name);
+        if (document.url) searchArray.push(document.url);
+      });
+    }
+
+    // Set the Search_value in the update object
+    update.Search_value = searchArray.filter(Boolean).join("+");
+    next();
+  } catch (error) {
+    next(error);
   }
+});
 
-  update.Search_value=searchArray.filter(Boolean).join('+')
-  next()
-
-})
 
 const UserSchema=new mongoose.Schema({
   userId:{type:String,required:true},
